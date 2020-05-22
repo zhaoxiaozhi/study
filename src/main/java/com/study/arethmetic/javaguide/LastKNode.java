5b74d00ef285b585c2a3c220530b2911aa2c4a2b
赵晓智 <zhaoxiaozhi@yxxinzhili.onaliyun.com>
2020-05-22 18:32:29 +0800

    算法

---
+++ src/main/java/com/study/arethmetic/javaguide/LastKNode.java
@@ -0,0 +1,14 @@
+package com.study.arethmetic.javaguide;
+
+import com.study.arethmetic.javaguide.util.ListNode;
+
+public class LastKNode {
+
+    public ListNode FindKthToTail(ListNode head, int k){
+
+
+
+        return null;
+    }
+
+}
---
+++ src/main/java/com/study/arethmetic/javaguide/util/ListNode.java
@@ -0,0 +1,13 @@
+package com.study.arethmetic.javaguide.util;
+
+import java.util.List;
+
+public class ListNode {
+
+   public int val;
+   public ListNode next = null;
+   public ListNode(int val){
+        this.val = val;
+    }
+
+}
